6f4dd823f3e3a7f124c13e661f2ad0c19ef218a6
Ayrton Ricardo <pm@ayrtonricardo.me>
2026-04-18 19:49:48 +0200

    feat: add argument

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+import sys
 from dwinlcd import DWIN_LCD
 
 encoder_Pins = (26, 19)
 button_Pin = 13
-LCD_COM_Port = '/dev/ttyAMA0'
-API_Key = 'eb56bb488d3143708656f60074f70af0'
+LCD_COM_Port = sys.argv[0]
+# '/dev/ttyAMA0'
+API_Key = sys.argv[1]
+#'eb56bb488d3143708656f60074f70af0'
 
 DWINLCD = DWIN_LCD(
         LCD_COM_Port,
